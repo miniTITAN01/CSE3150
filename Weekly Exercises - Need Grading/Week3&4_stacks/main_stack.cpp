9d575e7027fd98f1276a9ebb201ac2145a7de570
ttt21001 <ttt21001@ttt21001-vm.cse.uconn.edu>
2024-02-10 23:14:16 -0500

    Week4 STL

---
+++ Weekly Exercises - Need Grading/Week3&4_stacks/main_stack.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream> 
+
+#include "readin_return_int_stack.h"
+
+using namespace std;
+
+int main(){
+
+    stack<int> s = get_ints_from_fie("input.txt")
+
+    return 0;
+}
---
+++ Weekly Exercises - Need Grading/Week3&4_stacks/readin_return_int_stack.h
@@ -0,0 +1,12 @@
+
+#ifdef _READIN_RETURN_INT_STACK_H
+#define _READIN_RETURN_INT_STACK_H
+
+#include <stack>
+#include <string>
+
+using namespace std;
+
+stack <int> get_ints_from_fie(string file_name)
+
+#endif
